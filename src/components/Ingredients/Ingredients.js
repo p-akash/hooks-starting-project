8e3dd71454f5b3ef1806ab729e3025b172dcdc3d
p-akash <patel.akash@outlook.com>
2019-11-12 10:49:03 +0530

    sending http requests

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,15 +7,26 @@ import IngredientList from "./IngredientList";
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
   const addIngredientHandler = ingredient => {
-    setIngredients(prevIngredients => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...ingredient }
-    ]);
+    fetch("https://react-hook-263eb.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify(ingredients),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(resposeData => {
+        setIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: resposeData.name, ...ingredient }
+        ]);
+      });
   };
   const removeItemHandler = id => {
     const updatedIngredients = ingredients.filter(d => d.id !== id);
     setIngredients(updatedIngredients);
   };
+
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
